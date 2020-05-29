884ac249de108c1b80b9e5b5bd9942383741c641
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-05-29 11:13:48 -0400

    finished project with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,7 @@
-//Adds to the infrastructure of the HTML dynamically
-const searchDiv = document.getElementsByClassName('search-container');
-const searchForm = document.createElement('form');
-const searchSearch = document.createElement('input');
-const searchSubmit = document.createElement('input');
 const galleryDiv = document.getElementById('gallery');
 let userArray = [];
 
-searchSearch.type = 'search';
-searchSearch.id = 'search-input';
-searchSearch.className = 'search-input';
-searchSearch.placeholder = 'Search...'
-searchSubmit.type = 'submit';
-searchSubmit.value = 'Search';
-searchSubmit.id = 'search-submit';
-searchSubmit.className = 'search-submit';
-
-searchDiv[0].appendChild(searchForm);
-searchForm.appendChild(searchSearch);
-searchForm.appendChild(searchSubmit);
-
-//Get and display 12 random users Image, First & Last, Email, City or Location
+//converts API to json and checks for an error
 function fetchData(url) {
   return fetch(url)
             //.then(checkStatus)
@@ -27,13 +9,17 @@ function fetchData(url) {
             .catch(error => console.log('Looks like theres an error', error))
 }
 
+//12 random users are pulled
 for(i = 0; i < 12; i += 1) {
   fetchData('https://randomuser.me/api/')
+    //pushes each object into an array
+    //generates a card for the object
     .then(data => {
         const user = data.results[0]
         generateCard(user)
         userArray.push(user);
     })
+    //displays a modal window when the object card is clicked
     .then(data => {
       if (userArray.length === 12){
         const cardArray = document.querySelectorAll('div.card');
@@ -56,6 +42,7 @@ for(i = 0; i < 12; i += 1) {
     })
   }
 
+  //dynamically creates a card for the API object
   function generateCard(user) {
     const cardDiv = document.createElement('div');
     const cardImgDiv = document.createElement('div');
@@ -86,67 +73,74 @@ for(i = 0; i < 12; i += 1) {
     cardInfoDiv.appendChild(nameh3);
     cardInfoDiv.appendChild(cardEmail);
     cardInfoDiv.appendChild(cardArea);
-}
+  }
 
-function generateModal(user){
+  //dynamically creates a modal window for the API object
+  function generateModal(user){
 
-  const modalContainerDiv = document.createElement('div');
-  const body = document.body;
-  const bodyScript = body.lastElementChild;
-  const modalDiv = document.createElement('div');
-  const modalButton = document.createElement('button');
-  const strong = document.createElement('strong');
-  const modalInfoDiv = document.createElement('div');
-  const modalImg = document.createElement('img');
-  const modalh3 = document.createElement('h3');
-  const modalEmail = document.createElement('p');
-  const modalCity = document.createElement('p');
-  const modalHr = document.createElement('hr');
-  const modalPhone = document.createElement('p');
-  const modalAddress = document.createElement('p');
-  const modalBirthday = document.createElement('p');
+    const modalContainerDiv = document.createElement('div');
+    const body = document.body;
+    const bodyScript = body.lastElementChild;
+    const modalDiv = document.createElement('div');
+    const modalButton = document.createElement('button');
+    const strong = document.createElement('strong');
+    const modalInfoDiv = document.createElement('div');
+    const modalImg = document.createElement('img');
+    const modalh3 = document.createElement('h3');
+    const modalEmail = document.createElement('p');
+    const modalCity = document.createElement('p');
+    const modalHr = document.createElement('hr');
+    const modalPhone = document.createElement('p');
+    const modalAddress = document.createElement('p');
+    const modalBirthday = document.createElement('p');
+    const dob =  user.dob.date.split('');
+    const birthday = dob.slice(0, 10).join('');
+    
 
-  modalContainerDiv.className = 'modal-container';
-  modalDiv.className = 'modal';
-  modalButton.type = 'button';
-  modalButton.id = 'modal-close-btn';
-  modalButton.className = 'modal-close-btn';
-  modalInfoDiv.className = 'modal-info-container';
-  modalImg.className = 'modal-img';
-  modalImg.src = user.picture.large;
-  modalImg.alt = 'profile picture';
-  modalh3.id = 'name';
-  modalh3.className = 'modal-name cap';
-  modalh3.textContent = `${user.name.first} ${user.name.last}`;
-  modalEmail.className = 'modal-text';
-  modalEmail.textContent = user.email;
-  modalCity.className = 'modal-text cap';
-  modalCity.textContent = user.location.city;
-  modalPhone.className = 'modal-text';
-  modalPhone.textContent = user.cell;
-  modalAddress.className = 'modal-text';
-  modalAddress.textContent = `${user.location.street.number} ${user.location.street.name},
-    ${user.location.city}, ${user.location.state} ${user.location.postcode}`;
-  modalBirthday.className = 'modal-text';
-  modalBirthday.textContent = `Birthday: ${user.dob.date}`;
+    modalContainerDiv.className = 'modal-container';
+    modalDiv.className = 'modal';
+    modalButton.type = 'button';
+    modalButton.id = 'modal-close-btn';
+    modalButton.className = 'modal-close-btn';
+    modalInfoDiv.className = 'modal-info-container';
+    modalImg.className = 'modal-img';
+    modalImg.src = user.picture.large;
+    modalImg.alt = 'profile picture';
+    modalh3.id = 'name';
+    modalh3.className = 'modal-name cap';
+    modalh3.textContent = `${user.name.first} ${user.name.last}`;
+    modalEmail.className = 'modal-text';
+    modalEmail.textContent = user.email;
+    modalCity.className = 'modal-text cap';
+    modalCity.textContent = user.location.city;
+    modalPhone.className = 'modal-text';
+    modalPhone.textContent = user.cell;
+    modalAddress.className = 'modal-text';
+    modalAddress.textContent = `${user.location.street.number} ${user.location.street.name},
+      ${user.location.city}, ${user.location.state} ${user.location.postcode}`;
+    modalBirthday.className = 'modal-text';
+    modalBirthday.textContent = `Birthday: ${birthday}`;
 
-  body.insertBefore(modalContainerDiv, bodyScript);
-  modalContainerDiv.appendChild(modalDiv);
-  modalDiv.appendChild(modalButton);
-  modalButton.appendChild(strong);
-  modalDiv.appendChild(modalInfoDiv);
-  modalInfoDiv.appendChild(modalImg);
-  modalInfoDiv.appendChild(modalh3);
-  modalInfoDiv.appendChild(modalEmail);
-  modalInfoDiv.appendChild(modalCity);
-  modalInfoDiv.appendChild(modalHr);
-  modalInfoDiv.appendChild(modalPhone);
-  modalInfoDiv.appendChild(modalAddress);
-  modalInfoDiv.appendChild(modalBirthday);
 
-  modalContainerDiv.style.display = '';
 
-  modalButton.addEventListener('click', e => {
-    modalContainerDiv.style.display = 'none';
-  })
-};
+
+    body.insertBefore(modalContainerDiv, bodyScript);
+    modalContainerDiv.appendChild(modalDiv);
+    modalDiv.appendChild(modalButton);
+    modalButton.appendChild(strong);
+    modalDiv.appendChild(modalInfoDiv);
+    modalInfoDiv.appendChild(modalImg);
+    modalInfoDiv.appendChild(modalh3);
+    modalInfoDiv.appendChild(modalEmail);
+    modalInfoDiv.appendChild(modalCity);
+    modalInfoDiv.appendChild(modalHr);
+    modalInfoDiv.appendChild(modalPhone);
+    modalInfoDiv.appendChild(modalAddress);
+    modalInfoDiv.appendChild(modalBirthday);
+
+    modalContainerDiv.style.display = '';
+
+    modalButton.addEventListener('click', e => {
+      modalContainerDiv.style.display = 'none';
+    })
+  };
